8249a61dcf90e32ed3b4fa13e72213872f36013b
gyague sonam <gyakhoe@gmail.com>
2022-05-11 15:21:19 +0530

    About page moved to Mobile UI

--- lib/about/views/about_page.dart
+++ lib/about/views/about_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/l10n/l10n.dart';
+import 'package:portfolio/utilities/open_functions.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -9,7 +10,9 @@ class AboutPage extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: SizedBox(
-        height: MediaQuery.of(context).size.height - 100,
+        height: isMobileView(context: context)
+            ? MediaQuery.of(context).size.height - 10
+            : MediaQuery.of(context).size.height - 100,
         width: MediaQuery.of(context).size.width,
         child: const _AboutView(),
       ),
@@ -24,7 +27,9 @@ class _AboutView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 100),
+      margin: isMobileView(context: context)
+          ? const EdgeInsets.symmetric(horizontal: 10)
+          : const EdgeInsets.symmetric(horizontal: 100),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -37,12 +42,12 @@ class _AboutView extends StatelessWidget {
                       color: Colors.white,
                     ),
               ),
-              const Expanded(
+              Expanded(
                 child: Divider(
                   color: Colors.white,
                   thickness: 2,
                   indent: 20,
-                  endIndent: 100,
+                  endIndent: isMobileView(context: context) ? 20 : 100,
                 ),
               ),
             ],
@@ -57,62 +62,78 @@ class _AboutView extends StatelessWidget {
                   child: Center(
                     child: Text(
                       l10n.aboutMeDetail,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6!
-                          .copyWith(color: Colors.white),
+                      style: isMobileView(context: context)
+                          ? Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.copyWith(color: Colors.white)
+                          : Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(color: Colors.white),
                     ),
                   ),
                 ),
               ),
-              Expanded(
-                flex: 2,
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: FractionallySizedBox(
-                          widthFactor: 0.9,
-                          heightFactor: 0.9,
-                          child: Container(
-                            margin: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.greenAccent,
-                                width: 5,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        child: FractionallySizedBox(
-                          widthFactor: 0.9,
-                          heightFactor: 0.9,
-                          child: Container(
-                            margin: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.redAccent,
-                                width: 5,
-                              ),
-                            ),
-                            child: Image.asset(
-                              l10n.aboutMeImage,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+              if (isMobileView(context: context))
+                Container()
+              else
+                _aboutMeImage(l10n),
+            ],
+          ),
+          if (isMobileView(context: context))
+            _aboutMeImage(l10n)
+          else
+            Container(),
+        ],
+      ),
+    );
+  }
+
+  Expanded _aboutMeImage(AppLocalizations l10n) {
+    return Expanded(
+      flex: 2,
+      child: SizedBox(
+        height: 400,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FractionallySizedBox(
+                widthFactor: 0.9,
+                heightFactor: 0.9,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.greenAccent,
+                      width: 5,
+                    ),
                   ),
                 ),
               ),
-            ],
-          )
-        ],
+            ),
+            Align(
+              child: FractionallySizedBox(
+                widthFactor: 0.9,
+                heightFactor: 0.9,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.redAccent,
+                      width: 5,
+                    ),
+                  ),
+                  child: Image.asset(
+                    l10n.aboutMeImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
